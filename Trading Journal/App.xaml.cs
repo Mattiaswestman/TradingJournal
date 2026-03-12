33620b5ba975128db012b887d2b2e9359785f484
Mattias Westman <mattiaswestman@outlook.com>
2026-03-12 17:52:39 +0100

    Initial commit.

---
+++ Trading Journal/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace Trading_Journal
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
---
+++ Trading Journal/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace Trading_Journal
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
